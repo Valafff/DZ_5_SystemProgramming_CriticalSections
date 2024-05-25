f188e0aaf1c8be11a015ca895350de163bbdd167
Vetal <vvvv@vvvv.vv>
2024-05-25 22:41:52 +0300

    Add project files.

---
+++ DZ_5_SystemProgramming_CriticalSections/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_5_SystemProgramming_CriticalSections
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
